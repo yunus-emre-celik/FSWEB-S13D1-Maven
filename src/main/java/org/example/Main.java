bb5cc335e422f946af7e7330f4bc19180ddac8b1
yunus-emre-celik <yunus.celik.dev@gmail.com>
2024-11-18 18:16:17 +0300

    Done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,26 +2,41 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(shouldWakeUp (true, 1));
+        System.out.println(shouldWakeUp (false, 2));
+        System.out.println(shouldWakeUp (true, 8));
+        System.out.println(shouldWakeUp (true, -1));
     }
 
     public static boolean shouldWakeUp(boolean isBarking, int clock) {
-        
+        if (clock < 0 || clock > 23 || !isBarking) {
+            return false;
+        }
+        return clock>=20 || clock<8;
     }
 
     public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
-       
+        return (firstAge > 13 && firstAge <= 19) || (secondAge > 13 && secondAge <= 19) || (thirdAge > 13 && thirdAge <= 19);
+
     }
 
     public static boolean isCatPlaying(boolean isSummer, int temp) {
-     
+    int lowestTemp = 25;
+    int highestTemp = isSummer ? 45 : 35;
+    return temp<=highestTemp && temp> lowestTemp;
+
     }
 
     public static double area(double width, double height) {
-       
+        if(width<0 || height<0){ return -1;}
+        return width*height;
+
     }
 
     public static double area(double radius) {
-       
-    }
-}
+        if (radius < 0) {
+            return -1;
+        }else{
+        return Math.PI * Math.pow(radius, 2);}
+    }}
+
